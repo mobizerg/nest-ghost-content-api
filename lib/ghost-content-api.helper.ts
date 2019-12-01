d81ba7a836649156a538e2c5a5a32d3313c65256
Mobizerg <mobizerg@gmail.com>
2019-12-02 01:17:11 +0530

    Fixed fields bug

--- lib/ghost-content-api.helper.ts
+++ lib/ghost-content-api.helper.ts
@@ -10,3 +10,6 @@ export function seoDescription(str: string): string {
   return (str != null && str.length > 156) ? extractWords(str, 156) : str;
 }
 
+export function trimLineBreaks(str: string): string {
+  return str.replace(/(\r\n|\n|\r)/gm,'');
+}
--- lib/params/find-one-options.param.ts
+++ lib/params/find-one-options.param.ts
@@ -6,7 +6,7 @@ export class FindOneOptions<T1, T2> {
   @Transform(value => value ? (Array.isArray(value) ? value.join(',') : value) : undefined, { toClassOnly: true })
   include?: T2 | T2[];
 
-  fields?: [keyof T1];
+  fields?: Array<keyof T1>;
 
   formats?: ContentFormat[];
 }
